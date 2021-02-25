ecfff0d9032bd80bf6df67a4e1a7e96476897365
MrGoodBytes <bytes@goodbytes.live>
2021-02-25 17:12:54 -0600

    Removed applications.commands.update per https://github.com/discord/discord-api-docs/issues/2606

--- src/Enums/OAuthScopes.php
+++ src/Enums/OAuthScopes.php
@@ -31,7 +31,6 @@ use Illuminate\Support\Arr;
  * @method static self ACTIVITIES_READ() allows your app to fetch data from a user's 'Now Playing/Recently Played' list - whitelist only
  * @method static self ACTIVITIES_WRITE() allows your app to update a user's activity - whitelist only (NOT REQUIRED FOR GAMESDK ACTIVITIY MANAGER) (Whitelist only)
  * @method static self APPLICATIONS_COMMANDS() allows your app to use Slash Commands in a guild
- * @method static self APPLICATIONS_COMMANDS_UPDATE() allows your app to update Slash Commands via this bearer token
  *
  * @todo Refactor these to proper camel case functions
  *
@@ -102,7 +101,6 @@ class OAuthScopes extends Enum
             'ACTIVITIES_READ' => 'activities.read',
             'ACTIVITIES_WRITE' => 'activities.write',
             'APPLICATIONS_COMMANDS' => 'applications.commands',
-            'APPLICATIONS_COMMANDS_UPDATE' => 'applications.commands.update',
         ];
     }
 
@@ -133,7 +131,6 @@ class OAuthScopes extends Enum
             'ACTIVITIES_READ' => 'ACTIVITIES_READ',
             'ACTIVITIES_WRITE' => 'ACTIVITIES_WRITE',
             'APPLICATIONS_COMMANDS' => 'APPLICATIONS_COMMANDS',
-            'APPLICATIONS_COMMANDS_UPDATE' => 'APPLICATIONS_COMMANDS_UPDATE',
         ];
     }
 }
