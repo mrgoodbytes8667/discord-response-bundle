0954411ebc42db844c2fdca5e6a71f656093a936
MrGoodBytes <bytes@goodbytes.live>
2021-02-25 17:16:13 -0600

    Merge OAuthScopes.php from branch 'slash' Updated version field

--- src/Enums/OAuthScopes.php
+++ src/Enums/OAuthScopes.php
@@ -30,10 +30,13 @@ use Illuminate\Support\Arr;
  * @method static self RELATIONSHIPS_READ() allows your app to know a user's friends and implicit relationships - whitelist only
  * @method static self ACTIVITIES_READ() allows your app to fetch data from a user's 'Now Playing/Recently Played' list - whitelist only
  * @method static self ACTIVITIES_WRITE() allows your app to update a user's activity - whitelist only (NOT REQUIRED FOR GAMESDK ACTIVITIY MANAGER) (Whitelist only)
+ * @method static self APPLICATIONS_COMMANDS() allows your app to use Slash Commands in a guild
  *
  * @todo Refactor these to proper camel case functions
  *
  * @link https://github.com/spatie/enum
+ *
+ * @version v0.6.0 As of 2021-02-25 Discord Documentation
  */
 class OAuthScopes extends Enum
 {
@@ -97,6 +100,7 @@ class OAuthScopes extends Enum
             'RELATIONSHIPS_READ' => 'relationships.read',
             'ACTIVITIES_READ' => 'activities.read',
             'ACTIVITIES_WRITE' => 'activities.write',
+            'APPLICATIONS_COMMANDS' => 'applications.commands',
         ];
     }
 
@@ -126,6 +130,7 @@ class OAuthScopes extends Enum
             'RELATIONSHIPS_READ' => 'RELATIONSHIPS_READ',
             'ACTIVITIES_READ' => 'ACTIVITIES_READ',
             'ACTIVITIES_WRITE' => 'ACTIVITIES_WRITE',
+            'APPLICATIONS_COMMANDS' => 'APPLICATIONS_COMMANDS',
         ];
     }
 }
