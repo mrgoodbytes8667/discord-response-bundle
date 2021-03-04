51e7836e0d199334b0780324407fefbcb02a55c8
MrGoodBytes <bytes@goodbytes.live>
2021-03-04 14:47:18 -0600

    Add PartialGuild deserialization test for v6 and v8

--- Tests/DeserializationTest.php
+++ Tests/DeserializationTest.php
@@ -4,6 +4,7 @@
 namespace Bytes\DiscordResponseBundle\Tests;
 
 
+use Bytes\DiscordResponseBundle\Objects\PartialGuild;
 use Bytes\DiscordResponseBundle\Objects\User;
 
 class DeserializationTest extends TestSerializationCase
@@ -26,5 +27,45 @@ class DeserializationTest extends TestSerializationCase
         $this->assertEquals($output->getLocale(), "en-US");
         $this->assertEquals($output->getMfaEnabled(), true);
         $this->assertNull($output->getEmail());
+    }
+
+    public function testPartialGuildV6Deserialization()
+    {
+        $serializer = $this->createSerializer();
+
+        /** @var PartialGuild $output */
+        $output = $serializer->deserialize(file_get_contents(self::getFixturesFile('partial-guild-v6.json')), PartialGuild::class, 'json');
+
+        $this->assertEquals("732307126930327628", $output->getId());
+        $this->assertEquals("PZjj2rimTJQeMYYTCKKw7siBknEUdE", $output->getName());
+        $this->assertEquals('cfbc5b55d417d6fee18c89c8122e7fe7', $output->getIcon());
+        $this->assertFalse($output->getOwner());
+        $this->assertEquals(372633152, $output->getPermissions());
+        $this->assertIsInt($output->getPermissions());
+
+        $this->assertNull($output->getMessage());
+        $this->assertNull($output->getCode());
+        $this->assertNull($output->getRetryAfter());
+        $this->assertNull($output->getGlobal());
+    }
+
+    public function testPartialGuildV8Deserialization()
+    {
+        $serializer = $this->createSerializer();
+
+        /** @var PartialGuild $output */
+        $output = $serializer->deserialize(file_get_contents(self::getFixturesFile('partial-guild-v8.json')), PartialGuild::class, 'json');
+
+        $this->assertEquals("732307126930327628", $output->getId());
+        $this->assertEquals("PZjj2rimTJQeMYYTCKKw7siBknEUdE", $output->getName());
+        $this->assertEquals('cfbc5b55d417d6fee18c89c8122e7fe7', $output->getIcon());
+        $this->assertFalse($output->getOwner());
+        $this->assertEquals('6815084096', $output->getPermissions());
+        $this->assertIsString($output->getPermissions());
+
+        $this->assertNull($output->getMessage());
+        $this->assertNull($output->getCode());
+        $this->assertNull($output->getRetryAfter());
+        $this->assertNull($output->getGlobal());
     }
 }
