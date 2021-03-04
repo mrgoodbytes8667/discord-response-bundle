246e4e55e2f446fd7fe3a2289f6cca623d9bf77e
MrGoodBytes <bytes@goodbytes.live>
2021-03-04 14:51:53 -0600

    Fix flipped assertions, add global check for null errors

--- Tests/DeserializationTest.php
+++ Tests/DeserializationTest.php
@@ -4,6 +4,7 @@
 namespace Bytes\DiscordResponseBundle\Tests;
 
 
+use Bytes\DiscordResponseBundle\Objects\Interfaces\ErrorInterface;
 use Bytes\DiscordResponseBundle\Objects\User;
 
 class DeserializationTest extends TestSerializationCase
@@ -15,16 +16,29 @@ class DeserializationTest extends TestSerializationCase
         /** @var User $output */
         $output = $serializer->deserialize(file_get_contents(self::getFixturesFile('me.json')), User::class, 'json');
 
-        $this->assertEquals($output->getId(), "225922625525845374");
-        $this->assertEquals($output->getUsername(), "Raymond");
-        $this->assertEquals($output->getAvatar(), 'mz9575g5p5v8pyo3d4ps8h4a');
-        $this->assertEquals($output->getDiscriminator(), "4421");
-        $this->assertEquals($output->getPublicFlags(), 0);
-        $this->assertEquals($output->getFlags(), 0);
-        $this->assertEquals($output->getBot(), true);
-        $this->assertEquals($output->getVerified(), true);
-        $this->assertEquals($output->getLocale(), "en-US");
-        $this->assertEquals($output->getMfaEnabled(), true);
+        $this->assertEquals("225922625525845374", $output->getId());
+        $this->assertEquals("Raymond", $output->getUsername());
+        $this->assertEquals('mz9575g5p5v8pyo3d4ps8h4a', $output->getAvatar());
+        $this->assertEquals("4421", $output->getDiscriminator());
+        $this->assertEquals(0, $output->getPublicFlags());
+        $this->assertEquals(0, $output->getFlags());
+        $this->assertEquals(true, $output->getBot());
+        $this->assertEquals(true, $output->getVerified());
+        $this->assertEquals("en-US", $output->getLocale());
+        $this->assertEquals(true, $output->getMfaEnabled());
         $this->assertNull($output->getEmail());
+
+        $this->checkForNullErrors($output);
+    }
+
+    /**
+     * @param ErrorInterface $output
+     */
+    protected function checkForNullErrors($output)
+    {
+        $this->assertNull($output->getMessage());
+        $this->assertNull($output->getCode());
+        $this->assertNull($output->getRetryAfter());
+        $this->assertNull($output->getGlobal());
     }
 }
