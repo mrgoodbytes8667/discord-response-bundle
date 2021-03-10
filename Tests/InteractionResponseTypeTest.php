179151d3fed01268d3304c5781f1eb167cabbd12
MrGoodBytes <bytes@goodbytes.live>
2021-03-10 14:46:42 -0600

    Reworked test to catch the deprecation notices

--- Tests/InteractionResponseTypeTest.php
+++ Tests/InteractionResponseTypeTest.php
@@ -4,19 +4,21 @@ namespace Bytes\DiscordResponseBundle\Tests;
 
 use Bytes\DiscordResponseBundle\Enums\InteractionResponseType;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 class InteractionResponseTypeTest extends TestCase
 {
+    use ExpectDeprecationTrait;
+
     /**
      * @dataProvider provideData
      *
      * @param InteractionResponseType $entry
      * @param string $label
      * @param int $value
-     * @param string|null $deprecation
-     * @param string|null $exception
      */
-    public function testEnum(InteractionResponseType $entry, string $label, int $value, ?string $deprecation, ?string $exception) {
+    public function testEnum(InteractionResponseType $entry, string $label, int $value)
+    {
         $this->assertNotNull($entry);
         $this->assertInstanceOf(InteractionResponseType::class, $entry);
         $this->assertEquals($label, $entry->label);
@@ -24,15 +26,63 @@ class InteractionResponseTypeTest extends TestCase
     }
 
     /**
+     *
+     */
+    public function testInvalidEnum()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $entry = new InteractionResponseType('abc123');
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testAcknowledge()
+    {
+        $this->expectDeprecation('Since mrgoodbytes8667/discord-response-bundle 0.7.0: The "%s()" method has been deprecated by Discord and will stop functioning on April 9, 2021.');
+        $entry = InteractionResponseType::acknowledge();
+
+        $this->assertNotNull($entry);
+        $this->assertInstanceOf(InteractionResponseType::class, $entry);
+        $this->assertEquals('acknowledge', $entry->label);
+        $this->assertEquals(2, $entry->value);
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testChannelMessage()
+    {
+        $this->expectDeprecation('Since mrgoodbytes8667/discord-response-bundle 0.7.0: The "%s()" method has been deprecated by Discord and will stop functioning on April 9, 2021.');
+        $entry = InteractionResponseType::channelMessage();
+
+        $this->assertNotNull($entry);
+        $this->assertInstanceOf(InteractionResponseType::class, $entry);
+        $this->assertEquals('channelMessage', $entry->label);
+        $this->assertEquals(3, $entry->value);
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testAcknowledgeWithSource()
+    {
+        $this->expectDeprecation('Since mrgoodbytes8667/discord-response-bundle 0.7.0: The "%s()" method has been deprecated by Discord and will stop functioning on April 9, 2021. Please use deferredChannelMessageWithSource() instead.');
+        $entry = InteractionResponseType::acknowledgeWithSource();
+
+        $this->assertNotNull($entry);
+        $this->assertInstanceOf(InteractionResponseType::class, $entry);
+        $this->assertEquals('deferredChannelMessageWithSource', $entry->label);
+        $this->assertEquals(5, $entry->value);
+    }
+
+    /**
      * @return \Generator
      */
-    public function provideData() {
-
-        yield ['entry' => InteractionResponseType::pong(), 'label' => 'pong', 'value' => 1, 'deprecation' => null, 'exception' => null];
-        yield ['entry' => InteractionResponseType::acknowledge(), 'label' => 'acknowledge', 'value' => 2, 'deprecation' => 'Since mrgoodbytes8667/discord-response-bundle 0.7.0: The "%s()" method has been deprecated by Discord and will stop functioning on April 9, 2021.', 'exception' => null];
-        yield ['entry' => InteractionResponseType::channelMessage(), 'label' => 'channelMessage', 'value' => 3, 'deprecation' => 'Since mrgoodbytes8667/discord-response-bundle 0.7.0: The "%s()" method has been deprecated by Discord and will stop functioning on April 9, 2021.', 'exception' => null];
-        yield ['entry' => InteractionResponseType::channelMessageWithSource(), 'label' => 'channelMessageWithSource', 'value' => 4, 'deprecation' => null, 'exception' => null];
-        yield ['entry' => InteractionResponseType::acknowledgeWithSource(), 'label' => 'deferredChannelMessageWithSource', 'value' => 5, 'deprecation' => 'Since mrgoodbytes8667/discord-response-bundle 0.7.0: The "%s()" method has been deprecated by Discord and will stop functioning on April 9, 2021. Please use deferredChannelMessageWithSource() instead.', 'exception' => null];
-        yield ['entry' => InteractionResponseType::deferredChannelMessageWithSource(), 'label' => 'deferredChannelMessageWithSource', 'value' => 5, 'deprecation' => null, 'exception' => null];
+    public function provideData()
+    {
+        yield ['entry' => InteractionResponseType::pong(), 'label' => 'pong', 'value' => 1];
+        yield ['entry' => InteractionResponseType::channelMessageWithSource(), 'label' => 'channelMessageWithSource', 'value' => 4];
+        yield ['entry' => InteractionResponseType::deferredChannelMessageWithSource(), 'label' => 'deferredChannelMessageWithSource', 'value' => 5];
     }
 }
