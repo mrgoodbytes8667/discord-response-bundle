e114a52d2429fa8c69cb6ccd19174d6506fb801d
MrGoodBytes <bytes@goodbytes.live>
2021-03-24 15:53:28 -0500

    Update Overwrite model to be v6 and v8 compatible

---
+++ Tests/Objects/OverwriteTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Bytes\DiscordResponseBundle\Tests\Objects;
+
+use Bytes\DiscordResponseBundle\Objects\Overwrite;
+use Bytes\DiscordResponseBundle\Tests\TestDiscordFakerTrait;
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
+
+/**
+ * Class OverwriteTest
+ * @package Bytes\DiscordResponseBundle\Tests\Objects
+ */
+class OverwriteTest extends TestCase
+{
+    use TestDiscordFakerTrait, ExpectDeprecationTrait;
+
+    /**
+     * @dataProvider provideV6Overwrites
+     * @param string $id
+     * @param $type
+     * @param $allow
+     * @param $deny
+     * @param $allowNew
+     * @param $denyNew
+     *
+     * @group legacy
+     */
+    public function testGetSetTypeV6(string $id, $type, $allow, $deny, $allowNew, $denyNew)
+    {
+        $this->expectDeprecation('Since mrgoodbytes8667/discord-response-bundle 0.7.3: The "%s()" method has been deprecated by Discord and is not present in v8.');
+
+        $overwrite = new Overwrite();
+        $overwrite->setId($id);
+        $overwrite->setType($type);
+        $overwrite->setAllow($allow);
+        $overwrite->setDeny($deny);
+        $overwrite->setAllowNew($allowNew);
+        $overwrite->setDenyNew($denyNew);
+
+        $this->assertEquals($id, $overwrite->getId());
+        $this->assertEquals($type, $overwrite->getType());
+        $this->assertIsString($overwrite->getType());
+        $this->assertEquals($allow, $overwrite->getAllow());
+        $this->assertIsInt($overwrite->getAllow());
+        $this->assertEquals($deny, $overwrite->getDeny());
+        $this->assertIsInt($overwrite->getDeny());
+        $this->assertEquals($allowNew, $overwrite->getAllowNew());
+        $this->assertEquals($denyNew, $overwrite->getDenyNew());
+    }
+
+    /**
+     * @dataProvider provideV8Overwrites
+     * @param string $id
+     * @param $type
+     * @param $allow
+     * @param $deny
+     * @param $allowNew
+     * @param $denyNew
+     *
+     * @group legacy
+     */
+    public function testGetSetTypeV8(string $id, $type, $allow, $deny, $allowNew, $denyNew)
+    {
+        $this->expectDeprecation('Since mrgoodbytes8667/discord-response-bundle 0.7.3: The "%s()" method has been deprecated by Discord and is not present in v8.');
+
+        $overwrite = new Overwrite();
+        $overwrite->setId($id);
+        $overwrite->setType($type);
+        $overwrite->setAllow($allow);
+        $overwrite->setDeny($deny);
+
+        $this->assertEquals($id, $overwrite->getId());
+        $this->assertEquals($type, $overwrite->getType());
+        $this->assertIsInt($overwrite->getType());
+        $this->assertEquals($allow, $overwrite->getAllow());
+        $this->assertIsString($overwrite->getAllow());
+        $this->assertEquals($deny, $overwrite->getDeny());
+        $this->assertIsString($overwrite->getDeny());
+        $this->assertNull($overwrite->getAllowNew());
+        $this->assertNull($overwrite->getDenyNew());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideV6Overwrites()
+    {
+        $this->setupFaker();
+        foreach (range(1, 10) as $i) {
+            yield [
+                'id' => $this->faker->snowflake(),
+                'type' => $this->faker->randomElement(['role', 'member']),
+                'allow' => $this->faker->numberBetween(),
+                'deny' => $this->faker->numberBetween(),
+                'allowNew' => (string)$this->faker->numberBetween(),
+                'denyNew' => (string)$this->faker->numberBetween(),
+            ];
+        }
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideV8Overwrites()
+    {
+        $this->setupFaker();
+        foreach (range(1, 10) as $i) {
+            yield [
+                'id' => $this->faker->snowflake(),
+                'type' => $this->faker->numberBetween(0, 1),
+                'allow' => (string)$this->faker->numberBetween(),
+                'deny' => (string)$this->faker->numberBetween(),
+                'allowNew' => null,
+                'denyNew' => null,
+            ];
+        }
+    }
+}
--- src/Objects/Overwrite.php
+++ src/Objects/Overwrite.php
@@ -16,80 +16,145 @@ use Bytes\DiscordResponseBundle\Objects\Traits\IDTrait;
  * @link https://discord.com/developers/docs/resources/channel#overwrite-object
  *
  * @property string $id Role or User ID
+ *
+ * @version v0.7.3 As of 2021-03-24 Discord Documentation
  */
 class Overwrite implements ErrorInterface, IdInterface
 {
     use IDTrait, ErrorTrait;
 
     /**
-     * either "role" or "member"
-     * @var string|null
+     * v6: either "role" or "member"
+     * v8: either 0 (role) or 1 (member)
+     * @var string|int|null
      */
     private $type;
 
     /**
-     * permission bit set
-     * @var int|null
+     * v6: permission bit set (int)
+     * v8: permission bit set (string)
+     * @var string|int|null
      */
     private $allow;
 
     /**
-     * permission bit set
-     * @var int|null
+     * v6: permission bit set (int)
+     * v8: permission bit set (string)
+     * @var string|int|null
      */
     private $deny;
 
     /**
-     * @return string|null
+     * v6: permission bit set
+     * @var string|null
+     * @deprecated v0.7.3 Only applicable for v6 and will be removed
      */
-    public function getType(): ?string
+    private $allowNew;
+
+    /**
+     * v6: permission bit set
+     * @var string|null
+     * @deprecated v0.7.3 Only applicable for v6 and will be removed
+     */
+    private $denyNew;
+
+    /**
+     * @return int|string|null
+     */
+    public function getType()
     {
         return $this->type;
     }
 
     /**
-     * @param string|null $type
+     * @param int|string|null $type
      * @return $this
      */
-    public function setType(?string $type): self
+    public function setType($type): self
     {
         $this->type = $type;
         return $this;
     }
 
     /**
-     * @return int|null
+     * @return int|string|null
      */
-    public function getAllow(): ?int
+    public function getAllow()
     {
         return $this->allow;
     }
 
     /**
-     * @param int|null $allow
+     * @param int|string|null $allow
      * @return $this
      */
-    public function setAllow(?int $allow): self
+    public function setAllow($allow): self
     {
         $this->allow = $allow;
         return $this;
     }
 
     /**
-     * @return int|null
+     * @return int|string|null
      */
-    public function getDeny(): ?int
+    public function getDeny()
     {
         return $this->deny;
     }
 
     /**
-     * @param int|null $deny
+     * @param int|string|null $deny
      * @return $this
      */
-    public function setDeny(?int $deny): self
+    public function setDeny($deny): self
     {
         $this->deny = $deny;
         return $this;
     }
+
+    /**
+     * @return string|null
+     * @deprecated v0.7.3 Only applicable for v6 and will be removed
+     */
+    public function getAllowNew(): ?string
+    {
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.7.3', 'The "%s()" method has been deprecated by Discord and is not present in v8.', __METHOD__);
+        return $this->allowNew;
+    }
+
+    /**
+     * @param string|null $allowNew
+     * @return $this
+     * @deprecated v0.7.3 Only applicable for v6 and will be removed
+     */
+    public function setAllowNew(?string $allowNew): self
+    {
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.7.3', 'The "%s()" method has been deprecated by Discord and is not present in v8.', __METHOD__);
+        $this->allowNew = $allowNew;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     * @deprecated v0.7.3 Only applicable for v6 and will be removed
+     */
+    public function getDenyNew(): ?string
+    {
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.7.3', 'The "%s()" method has been deprecated by Discord and is not present in v8.', __METHOD__);
+        return $this->denyNew;
+    }
+
+    /**
+     * @param string|null $denyNew
+     * @return $this
+     * @deprecated v0.7.3 Only applicable for v6 and will be removed
+     */
+    public function setDenyNew(?string $denyNew): self
+    {
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.7.3', 'The "%s()" method has been deprecated by Discord and is not present in v8.', __METHOD__);
+        $this->denyNew = $denyNew;
+        return $this;
+    }
+
+
 }
