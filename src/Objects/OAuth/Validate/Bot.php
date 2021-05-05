67a0e5b1db6699525f96ffcd6744cddeace3d6bd
MrGoodBytes <bytes@goodbytes.live>
2021-05-05 10:34:30 -0500

    Add user, bot validation

---
+++ src/Objects/OAuth/Validate/Bot.php
@@ -0,0 +1,168 @@
+<?php
+
+
+namespace Bytes\DiscordResponseBundle\Objects\OAuth\Validate;
+
+
+use Bytes\DiscordResponseBundle\Objects\Application;
+use Bytes\ResponseBundle\Token\Interfaces\TokenValidationResponseInterface;
+use InvalidArgumentException;
+
+/**
+ * Class Bot
+ * @package Bytes\DiscordResponseBundle\Objects\OAuth\Validate
+ *
+ * @link https://discord.com/developers/docs/topics/oauth2#get-current-bot-application-information
+ *
+ * @version v0.9.0 As of 2021-05-05 Discord Documentation
+ */
+class Bot extends Application implements TokenValidationResponseInterface
+{
+    /**
+     * @param ...$args = ['id', 'name', 'icon', 'description', 'rpcOrigins', 'botPublic', 'botRequireCodeGrant', 'termsOfServiceUrl', 'privacyPolicyUrl', 'owner', 'summary', 'verifyKey']
+     * @return static
+     */
+    public static function create(...$args): static
+    {
+        $static = new static();
+
+        if (isset($args['id'])) {
+            $static->setId($args['id']);
+        }
+
+        if (isset($args['name'])) {
+            $static->setName($args['name']);
+        }
+
+        if (isset($args['icon'])) {
+            $static->setIcon($args['icon']);
+        }
+
+        if (isset($args['description'])) {
+            $static->setDescription($args['description']);
+        }
+
+        if (isset($args['rpcOrigins'])) {
+            $static->setRpcOrigins($args['rpcOrigins']);
+        }
+
+        if (isset($args['botPublic'])) {
+            $static->setBotPublic($args['botPublic']);
+        }
+
+        if (isset($args['botRequireCodeGrant'])) {
+            $static->setBotRequireCodeGrant($args['botRequireCodeGrant']);
+        }
+
+        if (isset($args['termsOfServiceUrl'])) {
+            $static->setTermsOfServiceUrl($args['termsOfServiceUrl']);
+        }
+
+        if (isset($args['privacyPolicyUrl'])) {
+            $static->setPrivacyPolicyUrl($args['privacyPolicyUrl']);
+        }
+
+        if (isset($args['owner'])) {
+            $static->setOwner($args['owner']);
+        }
+
+        if (isset($args['summary'])) {
+            $static->setSummary($args['summary']);
+        }
+
+        if (isset($args['verifyKey'])) {
+            $static->setVerifyKey($args['verifyKey']);
+        }
+        return $static;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getClientId(): ?string
+    {
+        return $this->getId();
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserName(): ?string
+    {
+        return $this->getName() ?? '';
+    }
+
+    /**
+     * @return string[]|null
+     */
+    public function getScopes(): ?array
+    {
+        return [];
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserId(): ?string
+    {
+        return $this->getId();
+    }
+
+    /**
+     * @param string|null $id
+     * @return bool
+     */
+    public function hasMatchingUserId(?string $id): bool
+    {
+        if (empty($this->getId()) || empty($id)) {
+            return false;
+        }
+        return $this->getId() === $id;
+    }
+
+    /**
+     * @param ...$args = ['clientId', 'userName']
+     * @return bool
+     */
+    public function isMatch(...$args): bool
+    {
+        return $this->hasMatchingClientId($args['clientId'] ?? null) &&
+            $this->hasMatchingUserName($args['userName'] ?? null);
+    }
+
+    /**
+     * @param string|null $clientId
+     * @return bool
+     */
+    public function hasMatchingClientId(?string $clientId): bool
+    {
+        if (empty($this->getId()) || empty($clientId)) {
+            return false;
+        }
+        return $this->getId() === $clientId;
+    }
+
+    /**
+     * @param string|null $userName
+     * @return bool
+     */
+    public function hasMatchingUserName(?string $userName): bool
+    {
+        if (empty($this->getName()) || empty($userName)) {
+            return false;
+        }
+        return $this->getName() === $userName;
+    }
+
+    /**
+     * Is this token an app/bot token?
+     * @return bool
+     *
+     * @throws InvalidArgumentException
+     */
+    public function isAppToken(): bool
+    {
+        return true;
+    }
+}
---
+++ src/Objects/OAuth/Validate/User.php
@@ -0,0 +1,214 @@
+<?php
+
+
+namespace Bytes\DiscordResponseBundle\Objects\OAuth\Validate;
+
+
+use Bytes\DiscordResponseBundle\Objects\Application;
+use Bytes\DiscordResponseBundle\Objects\Traits\ErrorTrait;
+use Bytes\DiscordResponseBundle\Objects\User as UserClass;
+use Bytes\ResponseBundle\Token\Interfaces\TokenValidationResponseInterface;
+use DateTimeImmutable;
+use DateTimeInterface;
+use InvalidArgumentException;
+
+/**
+ * Class User
+ * @package Bytes\DiscordResponseBundle\Objects\OAuth\Validate
+ *
+ * @link https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
+ *
+ * @version v0.9.0 As of 2021-05-05 Discord Documentation
+ */
+class User implements TokenValidationResponseInterface
+{
+    use ErrorTrait;
+
+    /**
+     * User constructor.
+     * @param Application|null $application
+     * @param array|null $scopes
+     * @param DateTimeInterface|null $expires
+     * @param UserClass|null $user
+     */
+    public function __construct(private ?Application $application = null, private ?array $scopes = [], private ?DateTimeInterface $expires = null, private ?UserClass $user = null)
+    {
+        if (empty($application)) {
+            $this->application = new Application();
+        }
+        if (empty($user)) {
+            $this->user = new UserClass();
+        }
+    }
+
+    /**
+     * @param ...$args = ['application', 'scopes', 'expires', 'user']
+     * @return static
+     */
+    public static function create(...$args): static
+    {
+        return new static(...$args);
+    }
+
+    /**
+     * @return Application|null
+     */
+    public function getApplication(): ?Application
+    {
+        return $this->application;
+    }
+
+    /**
+     * @param Application|null $application
+     * @return $this
+     */
+    public function setApplication(?Application $application): self
+    {
+        $this->application = $application;
+        return $this;
+    }
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getExpires(): ?DateTimeInterface
+    {
+        return $this->expires;
+    }
+
+    /**
+     * @param DateTimeInterface|null $expires
+     * @return $this
+     */
+    public function setExpires(?DateTimeInterface $expires): self
+    {
+        $this->expires = $expires;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasExpired()
+    {
+        return $this->expires <= new DateTimeImmutable();
+    }
+
+    /**
+     * @return UserClass|null
+     */
+    public function getUser(): ?UserClass
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param UserClass|null $user
+     * @return $this
+     */
+    public function setUser(?UserClass $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClientId(): ?string
+    {
+        return $this->application->getId();
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserName(): ?string
+    {
+        return $this->user?->getUsername();
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getScopes(): ?array
+    {
+        return $this->scopes;
+    }
+
+    /**
+     * @param array|null $scopes
+     * @return $this
+     */
+    public function setScopes(?array $scopes): self
+    {
+        $this->scopes = $scopes;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserId(): ?string
+    {
+        return $this->user?->getId();
+    }
+
+    /**
+     * @param ...$args
+     * @return bool
+     */
+    public function isMatch(...$args): bool
+    {
+        return $this->hasMatchingClientId($args['clientId'] ?? null) &&
+            $this->hasMatchingUserName($args['userName'] ?? null) &&
+            $this->hasMatchingUserId($args['userId'] ?? null);
+    }
+
+    /**
+     * @param string|null $clientId
+     * @return bool
+     */
+    public function hasMatchingClientId(?string $clientId): bool
+    {
+        if (empty($this->application?->getId()) || empty($clientId)) {
+            return false;
+        }
+        return $this->application?->getId() === $clientId;
+    }
+
+    /**
+     * @param string|null $userName
+     * @return bool
+     */
+    public function hasMatchingUserName(?string $userName): bool
+    {
+        if (empty($this->user?->getUsername()) || empty($userName)) {
+            return false;
+        }
+        return $this->user?->getUsername() === $userName;
+    }
+
+    /**
+     * @param string|null $id
+     * @return bool
+     */
+    public function hasMatchingUserId(?string $id): bool
+    {
+        if (empty($this->user?->getId()) || empty($id)) {
+            return false;
+        }
+        return $this->user?->getId() === $id;
+    }
+
+    /**
+     * Is this token an app/bot token?
+     * @return bool
+     *
+     * @throws InvalidArgumentException
+     */
+    public function isAppToken(): bool
+    {
+        return false;
+    }
+}
