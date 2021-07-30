354a742ede695e8869994f056178029891e86f16
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-07-30 13:05:07 -0500

    Add ApplicationCommandInterface (#21)

---
+++ src/Objects/Interfaces/ApplicationCommandInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bytes\DiscordResponseBundle\Objects\Interfaces;
+
+/**
+ * An application command is the base "command" model that belongs to an application. This is what you are creating when you POST a new command.
+ * A command, or each individual subcommand, can have a maximum of 25 options
+ *
+ * @link https://discord.com/developers/docs/interactions/slash-commands#applicationcommand
+ *
+ * @version v0.9.6 As of 2021-07-30 Discord Documentation
+ */
+interface ApplicationCommandInterface extends NameInterface
+{
+    /**
+     * @return string|null
+     */
+    public function getCommandId(): ?string;
+
+    /**
+     * @return bool|null
+     */
+    public function getDefaultPermission(): ?bool;
+
+    /**
+     * @param bool|null $default_permission
+     * @return $this
+     */
+    public function setDefaultPermission(?bool $default_permission);
+}
