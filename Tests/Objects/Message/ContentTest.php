4b55de13765292b5ee54e930a68e2c55a96e4cc2
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-08-06 09:32:08 -0500

    Updated Content model (#28)
    * Updated Content model
    
    * Added new() method to replace create(), in next release, create() will take on the signature of new()
    
    * Deprecated getEmbed(), setEmbed(), create()

---
+++ Tests/Objects/Message/ContentTest.php
@@ -0,0 +1,443 @@
+<?php
+
+namespace Bytes\DiscordResponseBundle\Tests\Objects\Message;
+
+use Bytes\Common\Faker\Discord\TestDiscordFakerTrait;
+use Bytes\Common\Faker\Providers\Discord;
+use Bytes\DiscordResponseBundle\Enums\ComponentType;
+use Bytes\DiscordResponseBundle\Objects\Embed\Embed;
+use Bytes\DiscordResponseBundle\Objects\Message\AllowedMentions;
+use Bytes\DiscordResponseBundle\Objects\Message\Component;
+use Bytes\DiscordResponseBundle\Objects\Message\Content;
+use Bytes\DiscordResponseBundle\Objects\MessageReference;
+use Bytes\DiscordResponseBundle\Tests\TestRolesSerializationCase;
+use Bytes\Tests\Common\DataProvider\BooleanProviderTrait;
+use Bytes\Tests\Common\DataProvider\NullProviderTrait;
+use Faker\Factory;
+use Faker\Generator as FakerGenerator;
+use Faker\Provider\Base;
+use Generator;
+use Illuminate\Support\Arr;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
+use Symfony\Component\String\ByteString;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+/**
+ *
+ */
+class ContentTest extends TestRolesSerializationCase
+{
+    use TestDiscordFakerTrait, ExpectDeprecationTrait, BooleanProviderTrait, NullProviderTrait;
+
+    /**
+     * @group legacy
+     */
+    public function testLegacyValidationPass()
+    {
+        $this->validationPass([
+            Content::create($this->faker->embed(), ByteString::fromRandom(2000))
+        ]);
+    }
+
+    /**
+     *
+     */
+    public function testValidationPass()
+    {
+        $this->validationPass([
+            Content::new($this->faker->embed(), ByteString::fromRandom(2000))
+        ]);
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testLegacyValidationFail()
+    {
+        $content = new Content();
+        $this->validationFail([
+            Content::create($this->faker->embed(), ByteString::fromRandom(2001)),
+            $content
+        ]);
+    }
+
+    /**
+     * @dataProvider provideExcessStickerIds
+     * @param $stickers
+     */
+    public function testValidationFail($stickers)
+    {
+        $content = new Content();
+        $content->setStickerIds($stickers);
+        $this->validationFail([
+            Content::new($this->faker->embed(), ByteString::fromRandom(2001)), // Content cannot be more than 2000 characters
+            new Content(), // Either content, embeds, components, or stickers must be provided
+            $content // cannot have more than 3 sticker ids
+        ]);
+    }
+
+    /**
+     * @dataProvider provideExcessStickerIds
+     * @param $stickers
+     */
+    public function testTooManyStickers($stickers)
+    {
+        $content = Content::new(stickers: $stickers);
+        try {
+            $this->validate($content);
+        } catch (ValidationFailedException $exception) {
+            $this->assertEquals(1, count($exception->getViolations()));
+
+            /** @var ConstraintViolationInterface $violation */
+            $violation = $exception->getViolations()[0];
+            $this->assertEquals('You cannot specify more than 3 stickers per message.', $violation->getMessage());
+            $this->assertEquals('sticker_ids', $violation->getPropertyPath());
+        }
+    }
+
+    /**
+     *
+     */
+    public function testInvalidComponents()
+    {
+        $content = Content::new(components: [new Embed()]);
+        try {
+            $this->validate($content);
+        } catch (ValidationFailedException $exception) {
+            $this->assertEquals(2, count($exception->getViolations()));
+
+            /** @var ConstraintViolationInterface $violation */
+            $violation = $exception->getViolations()[0];
+            $this->assertEquals('Either content, embeds, or stickers must be populated.', $violation->getMessage());
+            $this->assertEquals('content', $violation->getPropertyPath());
+
+            $violation = $exception->getViolations()[1];
+            $this->assertEquals('This value should be of type \Bytes\DiscordResponseBundle\Objects\Message\Component.', $violation->getMessage());
+            $this->assertStringStartsWith('components', $violation->getPropertyPath());
+        }
+    }
+
+    /**
+     * @dataProvider provideContent
+     */
+    public function testGetSetContent($input)
+    {
+        $content = new Content();
+        $this->assertNull($content->getContent());
+        $this->assertInstanceOf(Content::class, $content->setContent(null));
+        $this->assertNull($content->getContent());
+        $this->assertInstanceOf(Content::class, $content->setContent($input));
+        $this->assertEquals($input, $content->getContent());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideContent()
+    {
+        $faker = Factory::create();
+
+        yield [$faker->sentence()];
+        yield [''];
+        yield [null];
+    }
+
+    /**
+     * @dataProvider provideNonce
+     */
+    public function testGetSetNonce($nonce)
+    {
+        $content = new Content();
+        $this->assertNull($content->getNonce());
+        $this->assertInstanceOf(Content::class, $content->setNonce(null));
+        $this->assertNull($content->getNonce());
+        $this->assertInstanceOf(Content::class, $content->setNonce($nonce));
+        $this->assertEquals($nonce, $content->getNonce());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideNonce()
+    {
+        /** @var FakerGenerator|Base $faker */
+        $faker = Factory::create();
+
+        yield [$faker->randomLetter()];
+        yield [$faker->randomNumber()];
+        yield [''];
+        yield [null];
+    }
+
+    /**
+     * @dataProvider provideBooleans
+     * @dataProvider provideNull
+     */
+    public function testGetSetTts($tts)
+    {
+        $content = new Content();
+        $this->assertNull($content->getTts());
+        $this->assertInstanceOf(Content::class, $content->setTts(null));
+        $this->assertNull($content->getTts());
+        $this->assertInstanceOf(Content::class, $content->setTts($tts));
+        $this->assertEquals($tts, $content->getTts());
+    }
+
+    /**
+     * @dataProvider provideEmbed
+     * @group legacy
+     */
+    public function testGetSetEmbed($embed)
+    {
+        $this->expectDeprecation('');
+        $content = new Content();
+        $this->assertNull($content->getEmbed());
+        $this->assertInstanceOf(Content::class, $content->setEmbed(null));
+        $this->assertNull($content->getEmbed());
+        $this->assertInstanceOf(Content::class, $content->setEmbed($embed));
+        $this->assertEquals($embed, $content->getEmbed());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideEmbed()
+    {
+        $this->setupFaker();
+        yield [$this->faker->embed()];
+        yield [null];
+    }
+
+    /**
+     * @dataProvider provideEmbeds
+     * @param $embeds
+     * @param $count
+     */
+    public function testGetSetEmbeds($embeds, $count)
+    {
+        $content = new Content();
+        $this->assertCount(0, $content->getEmbeds());
+        $this->assertInstanceOf(Content::class, $content->setEmbeds($embeds));
+        $this->assertCount($count, $content->getEmbeds());
+        $this->assertEquals($embeds, $content->getEmbeds());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideEmbeds()
+    {
+        $this->setupFaker();
+        yield ['embeds' => [$this->faker->embed()], 'count' => 1];
+        yield ['embeds' => [$this->faker->embed(), $this->faker->embed()], 'count' => 2];
+        yield ['embeds' => [$this->faker->embed(), $this->faker->embed(), $this->faker->embed()], 'count' => 3];
+        yield ['embeds' => [], 'count' => 0];
+    }
+
+    /**
+     * @dataProvider provideAllowedMentions
+     */
+    public function testGetSetAllowedMentions($allowedMentions)
+    {
+        $content = new Content();
+        $this->assertNull($content->getAllowedMentions());
+        $this->assertInstanceOf(Content::class, $content->setAllowedMentions(null));
+        $this->assertNull($content->getAllowedMentions());
+        $this->assertInstanceOf(Content::class, $content->setAllowedMentions($allowedMentions));
+        $this->assertEquals($allowedMentions, $content->getAllowedMentions());
+    }
+
+    /**
+     * @dataProvider provideMessageReference
+     */
+    public function testGetSetMessageReference($messageReference)
+    {
+        $content = new Content();
+        $this->assertNull($content->getMessageReference());
+        $this->assertInstanceOf(Content::class, $content->setMessageReference(null));
+        $this->assertNull($content->getMessageReference());
+        $this->assertInstanceOf(Content::class, $content->setMessageReference($messageReference));
+        $this->assertEquals($messageReference, $content->getMessageReference());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideMessageReference()
+    {
+        /** @var FakerGenerator|Discord $faker */
+        $faker = Factory::create();
+        $faker->addProvider(new Discord($faker));
+
+        foreach ([$faker->snowflake(), null] as $channel) {
+            foreach ([$faker->snowflake(), null] as $guild) {
+                foreach ([true, false] as $failFlag) {
+                    yield [MessageReference::create($faker->snowflake(), $channel, $guild, $failFlag)];
+                }
+            }
+        }
+    }
+
+    /**
+     * @dataProvider provideStickerIds
+     * @param $stickers
+     * @param $count
+     */
+    public function testGetSetStickerIds($stickers, $count)
+    {
+        $content = new Content();
+        $this->assertCount(0, $content->getStickerIds());
+        $this->assertInstanceOf(Content::class, $content->setStickerIds($stickers));
+        $this->assertCount($count, $content->getStickerIds());
+        $this->assertEquals($stickers, $content->getStickerIds());
+        $this->assertInstanceOf(Content::class, $content->addStickerId($stickers[0]));
+        $this->assertCount($count, $content->getStickerIds());
+        $this->assertInstanceOf(Content::class, $content->addStickerId($this->faker->snowflake()));
+        $this->assertCount($count + 1, $content->getStickerIds());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideStickerIds()
+    {
+        $this->setupFaker();
+        yield ['stickers' => [$this->faker->snowflake()], 'count' => 1];
+        yield ['stickers' => [$this->faker->snowflake(), $this->faker->snowflake()], 'count' => 2];
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideExcessStickerIds()
+    {
+        $this->setupFaker();
+        $stickers = [];
+        foreach (range(1, 5) as $i) {
+            $stickers[] = $this->faker->snowflake();
+        }
+        yield [$stickers];
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideCreate()
+    {
+        $this->setupFaker();
+        $embed = $this->faker->embed();
+
+        foreach ([$this->faker->sentence(), '', null] as $content) {
+            foreach ($this->provideAllowedMentions() as $provideAllowedMention) {
+                $allowedMention = $provideAllowedMention[0];
+                foreach (array_merge(Arr::flatten($this->provideBooleans()), Arr::flatten($this->provideNull())) as $tts) {
+                    yield ['object' => Content::new($embed, $content, $allowedMention, tts: $tts), 'embed' => $embed, 'content' => $content, 'allowedMentions' => $allowedMention, 'tts' => $tts];
+                    yield ['object' => Content::new($embed, $content, $allowedMention), 'embed' => $embed, 'content' => $content, 'allowedMentions' => $allowedMention, 'tts' => null];
+                    yield ['object' => Content::new($embed, $content), 'embed' => $embed, 'content' => $content, 'allowedMentions' => null, 'tts' => null];
+                    yield ['object' => Content::new($embed), 'embed' => $embed, 'content' => null, 'allowedMentions' => null, 'tts' => null];
+                }
+            }
+        }
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideAllowedMentions()
+    {
+        /** @var FakerGenerator|Base $faker */
+        $faker = Factory::create();
+
+        yield [AllowedMentions::createForEveryone()];
+
+        yield [AllowedMentions::createForRoles($this->generateFakeRoles($faker->randomDigitNotNull()))];
+
+        yield [AllowedMentions::create()];
+
+        yield [null];
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideTts()
+    {
+        yield [true];
+        yield [false];
+        yield [null];
+    }
+
+    /**
+     * @dataProvider provideComponents
+     * @param mixed $components
+     */
+    public function testGetSetAddComponents($components)
+    {
+        $component = new Component();
+        $component->setUrl($this->faker->url());
+        $content = new Content();
+        $this->assertCount(0, $content->getComponents());
+        $this->assertInstanceOf(Content::class, $content->setComponents($components));
+        $this->assertEquals($components, $content->getComponents());
+        $this->assertCount(1, $content->getComponents());
+        $this->assertInstanceOf(Content::class, $content->addComponent($components[0]));
+        $this->assertCount(1, $content->getComponents());
+        $this->assertInstanceOf(Content::class, $content->addComponent($component));
+        $this->assertCount(2, $content->getComponents());
+        $this->assertInstanceOf(Content::class, $content->addComponent($component));
+        $this->assertCount(2, $content->getComponents());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideComponents()
+    {
+        $this->setupFaker();
+        yield [[new Component()]];
+    }
+
+    /**
+     * @dataProvider provideCreate
+     * @param Content $object
+     * @param $embed
+     * @param $content
+     * @param $allowedMentions
+     * @param $tts
+     */
+    public function testCreate(Content $object, $embed, $content, $allowedMentions, $tts)
+    {
+        $resultEmbeds = $object->getEmbeds();
+        $this->assertCount(1, $resultEmbeds);
+
+        $resultEmbed = array_shift($resultEmbeds);
+        $this->assertEquals($embed, $resultEmbed);
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testLegacyCreateManual()
+    {
+        $embed = $this->faker->embed();
+        $content = Content::create(embed: $embed, tts: true);
+
+        $this->assertCount(1, $content->getEmbeds());
+        $this->assertTrue($content->getTts());
+    }
+
+    /**
+     *
+     */
+    public function testCreateManual()
+    {
+        $embed = $this->faker->embed();
+        $component = Component::create(ComponentType::actionRow());
+        $content = Content::new(embeds: $embed, components: $component, tts: true);
+
+        $this->assertCount(1, $content->getEmbeds());
+        $this->assertCount(1, $content->getComponents());
+        $this->assertTrue($content->getTts());
+    }
+}
--- src/Objects/Message/Content.php
+++ src/Objects/Message/Content.php
@@ -4,15 +4,22 @@
 namespace Bytes\DiscordResponseBundle\Objects\Message;
 
 
-use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Bytes\DiscordResponseBundle\Objects\Embed\Embed;
 use Bytes\DiscordResponseBundle\Objects\MessageReference;
+use Doctrine\Common\Collections\ArrayCollection;
+use Illuminate\Support\Arr;
+use JetBrains\PhpStorm\Deprecated;
 use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * Class Content
  * @package Bytes\DiscordResponseBundle\Objects\Message
+ *
+ * @link https://discord.com/developers/docs/resources/channel#create-message-jsonform-params
+ *
+ * @version v0.9.12 As of 2021-08-05 Discord Documentation
  */
 class Content
 {
@@ -33,22 +40,63 @@ class Content
      * @var boolean|null
      */
     private $tts;
-    
+
     /**
-     * @var Embed|null
+     * @var Embed[]|null
+     * @Assert\Valid()
+     * @Assert\All({
+     *     @Assert\Type(type="\Bytes\DiscordResponseBundle\Objects\Embed\Embed"),
+     *     @Assert\NotNull()
+     * })
      */
-    private $embed;
+    private $embeds;
 
     /**
      * @var AllowedMentions|null
      * @SerializedName("allowed_mentions")
+     * @Assert\Valid()
      */
     private $allowedMentions;
 
     /**
      * @var MessageReference|null
+     * @Assert\Valid()
      */
     private $messageReference;
+
+    /**
+     * @var Component[]|null
+     * @Assert\Valid()
+     * @Assert\All({
+     *     @Assert\Type(type="\Bytes\DiscordResponseBundle\Objects\Message\Component"),
+     *     @Assert\NotNull()
+     * })
+     */
+    private $components;
+
+    /**
+     * @var string[]|null
+     * @Assert\Count(
+     *      max = 3,
+     *      maxMessage = "You cannot specify more than {{ limit }} stickers per message."
+     * )
+     * @Assert\All({
+     *     @Assert\Type(type="string"),
+     *     @Assert\NotNull()
+     * })
+     */
+    private $sticker_ids;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->embeds = new ArrayCollection();
+        $this->components = new ArrayCollection();
+        $this->sticker_ids = new ArrayCollection();
+    }
+
 
     /**
      * @return string|null
@@ -107,18 +155,51 @@ class Content
     /**
      * @return Embed|null
      */
+    #[Deprecated(reason: 'since 0.9.12, use getEmbeds() instead (deprecated in Discord API v9)', replacement: '%class%->getEmbeds()')]
     public function getEmbed(): ?Embed
     {
-        return $this->embed;
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.9.12', 'Use getEmbeds() instead.');
+        return $this->embeds->first() ?: null;
     }
 
     /**
      * @param Embed|null $embed
      * @return $this
      */
+    #[Deprecated(reason: 'since 0.9.12, use setEmbeds() instead (deprecated in Discord API v9)', replacement: '%class%->setEmbeds(%parametersList%)')]
     public function setEmbed(?Embed $embed): self
     {
-        $this->embed = $embed;
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.9.12', 'Use setEmbeds() instead.');
+        return $this->setEmbeds([$embed]);
+    }
+
+    /**
+     * @return Embed[]|null
+     */
+    public function getEmbeds(): ?array
+    {
+        return $this->embeds->toArray();
+    }
+
+    /**
+     * @param Embed[]|null $embeds
+     * @return Content
+     */
+    public function setEmbeds(?array $embeds): self
+    {
+        $this->embeds = new ArrayCollection($embeds);
+        return $this;
+    }
+
+    /**
+     * @param Embed $embed
+     * @return $this
+     */
+    public function addEmbed(Embed $embed): self
+    {
+        if (!$this->embeds->contains($embed)) {
+            $this->embeds->add($embed);
+        }
         return $this;
     }
 
@@ -159,27 +240,120 @@ class Content
     }
 
     /**
+     * @return Component[]|null
+     */
+    public function getComponents(): ?array
+    {
+        return $this->components->toArray();
+    }
+
+    /**
+     * @param Component[]|null $components
+     * @return $this
+     */
+    public function setComponents(?array $components): self
+    {
+        $this->components = new ArrayCollection($components);
+        return $this;
+    }
+
+    /**
+     * @param Component $component
+     * @return $this
+     */
+    public function addComponent(Component $component): self
+    {
+        if (!$this->components->contains($component)) {
+            $this->components->add($component);
+        }
+        return $this;
+    }
+
+    /**
+     * @return string[]|null
+     */
+    public function getStickerIds(): ?array
+    {
+        return $this->sticker_ids->toArray();
+    }
+
+    /**
+     * @param string[]|null $stickerIds
+     * @return $this
+     */
+    public function setStickerIds(?array $stickerIds): self
+    {
+        $this->sticker_ids = new ArrayCollection($stickerIds);
+        return $this;
+    }
+
+    /**
+     * @param string $stickerId
+     * @return $this
+     */
+    public function addStickerId(string $stickerId): self
+    {
+        if (!$this->sticker_ids->contains($stickerId)) {
+            $this->sticker_ids->add($stickerId);
+        }
+        return $this;
+    }
+
+    /**
      * @param Embed $embed
      * @param string|null $content
      * @param AllowedMentions|null $allowedMentions
      * @param bool|null $tts
      * @return static
      */
+    #[Deprecated(reason: 'since 0.9.12, use "new()" instead ("create()" will change to use the same arguments as "new()" in v0.10).', replacement: '%class%::new([%parameter0%], %parameter1%, %parameter2%, tts: %parameter3%)')]
     public static function create(Embed $embed, ?string $content = null, ?AllowedMentions $allowedMentions = null, ?bool $tts = null)
     {
-        if(empty($allowedMentions))
-        {
+        trigger_deprecation('mrgoodbytes8667/discord-response-bundle', '0.9.12', 'Use "new()" instead ("create()" will change to use the same arguments as "new()" in v0.10).');
+        if (empty($allowedMentions)) {
             $allowedMentions = AllowedMentions::create();
         }
         $static = new static();
-        $static->setEmbed($embed);
+        $static->addEmbed($embed);
         $static->setAllowedMentions($allowedMentions);
-        if(!empty($content))
-        {
+        if (!empty($content)) {
             $static->setContent($content);
         }
-        if(!is_null($tts))
-        {
+        if (!is_null($tts)) {
+            $static->setTts($tts);
+        }
+        return $static;
+    }
+
+    /**
+     * @param Embed|array|null $embeds
+     * @param string|null $content
+     * @param AllowedMentions|null $allowedMentions
+     * @param Component|array|null $components
+     * @param string[]|null $stickers
+     * @param bool|null $tts
+     * @return static
+     */
+    public static function new(Embed|array|null $embeds = null, ?string $content = null, ?AllowedMentions $allowedMentions = null, Component|array|null $components = null, ?array $stickers = null, ?bool $tts = null): static
+    {
+        if (empty($allowedMentions)) {
+            $allowedMentions = AllowedMentions::create();
+        }
+        $static = new static();
+        if (!empty($embeds)) {
+            $static->setEmbeds(Arr::wrap($embeds));
+        }
+        if (!empty($components)) {
+            $static->setComponents(Arr::wrap($components));
+        }
+        $static->setAllowedMentions($allowedMentions);
+        if (!empty($content)) {
+            $static->setContent($content);
+        }
+        if (!is_null($stickers)) {
+            $static->setStickerIds($stickers);
+        }
+        if (!is_null($tts)) {
             $static->setTts($tts);
         }
         return $static;
@@ -192,8 +366,8 @@ class Content
      */
     public function validate(ExecutionContextInterface $context, $payload)
     {
-        if (empty($this->content) && empty($this->embed)) {
-            $context->buildViolation('Either content or embed must be populated.')
+        if (empty($this->content) && $this->embeds->isEmpty() && $this->sticker_ids->isEmpty()) {
+            $context->buildViolation('Either content, embeds, or stickers must be populated.')
                 ->atPath('content')
                 ->addViolation();
         }
