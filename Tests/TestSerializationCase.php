d803b9ae4f14f72e97fd5372f79544e1d9952fc6
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-05-08 15:14:53 -0500

    Update base test cases to match the other repos

--- Tests/TestSerializationCase.php
+++ Tests/TestSerializationCase.php
@@ -4,49 +4,15 @@
 namespace Bytes\DiscordResponseBundle\Tests;
 
 
-use Bytes\EnumSerializerBundle\Serializer\Normalizer\EnumNormalizer;
+use Bytes\Tests\Common\TestSerializerTrait;
 use Bytes\Tests\Common\TestValidatorTrait;
-use Doctrine\Common\Annotations\AnnotationReader;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
-use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
-use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
-use Symfony\Component\PropertyInfo\Extractor\SerializerExtractor;
-use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Mapping\ClassDiscriminatorFromClassMetadata;
-use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
-use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
-use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
-use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\ConstraintViolationListNormalizer;
-use Symfony\Component\Serializer\Normalizer\DataUriNormalizer;
-use Symfony\Component\Serializer\Normalizer\DateIntervalNormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeZoneNormalizer;
-use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Normalizer\ProblemNormalizer;
-use Symfony\Component\Serializer\Normalizer\UnwrappingDenormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Validator\Validator\RecursiveValidator;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
-abstract class TestSerializationCase extends TestCase
+abstract class TestSerializationCase extends FixtureTestCase
 {
-    use TestValidatorTrait;
-
-    /**
-     * @param string $file
-     * @return string
-     */
-    public static function getFixturesFile(string $file)
-    {
-        return __DIR__ . '/Fixtures/' . $file;
-    }
+    use TestValidatorTrait, TestSerializerTrait;
 
     /**
      * @param $validations
@@ -86,78 +52,6 @@ abstract class TestSerializationCase extends TestCase
             $violations = $validator->validate($validation);
             $this->assertGreaterThanOrEqual(1, count($violations));
         }
-    }
-
-    protected function createSerializer(bool $includeEnumNormalizer = true, bool $includeObjectNormalizer = true, array $prependNormalizers = [], array $appendNormalizers = [])
-    {
-        if (empty($appendNormalizers)) {
-            $appendNormalizers = [
-                new ProblemNormalizer(),
-                new JsonSerializableNormalizer(),
-                new DateTimeNormalizer(),
-                new ConstraintViolationListNormalizer(),
-                new DateTimeZoneNormalizer(),
-                new DateIntervalNormalizer(),
-                new DataUriNormalizer(),
-                new ArrayDenormalizer(),
-            ];
-        }
-
-        $encoders = [new XmlEncoder(), new JsonEncoder(), new CsvEncoder()];
-        $normalizers = $this->getNormalizers($includeEnumNormalizer, $includeObjectNormalizer, array_merge($prependNormalizers, [new UnwrappingDenormalizer()]), $appendNormalizers);
-
-        return new Serializer($normalizers, $encoders);
-    }
-
-    /**
-     * @param bool $includeEnumNormalizer
-     * @param bool $includeObjectNormalizer
-     * @param array $prependNormalizers
-     * @param array $appendNormalizers
-     * @return array
-     */
-    protected function getNormalizers(bool $includeEnumNormalizer = true, bool $includeObjectNormalizer = true, array $prependNormalizers = [], array $appendNormalizers = [])
-    {
-        $normalizers = $prependNormalizers;
-        if ($includeEnumNormalizer || $includeObjectNormalizer || !empty($appendNormalizers)) {
-            $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
-            $metadataAwareNameConverter = new MetadataAwareNameConverter($classMetadataFactory);
-
-            $serializerExtractor = new SerializerExtractor($classMetadataFactory);
-            $phpDocExtractor = new PhpDocExtractor();
-            $reflectionExtractor = new ReflectionExtractor();
-
-            // list: SerializerExtractor, ReflectionExtractor, DoctrineExtractor
-            // type: Doctrine, PhpDoc, Reflection
-            // description: PhpDoc
-            // access: Doctrine, Reflection
-            // init: Reflection
-            $propertyInfo = new PropertyInfoExtractor(
-                [$serializerExtractor, $reflectionExtractor],
-                [$phpDocExtractor, $reflectionExtractor],
-                [$phpDocExtractor],
-                [$reflectionExtractor],
-                [$reflectionExtractor]
-            );
-
-            $objectNormalizer = new ObjectNormalizer($classMetadataFactory, $metadataAwareNameConverter, new PropertyAccessor(), $propertyInfo, new ClassDiscriminatorFromClassMetadata($classMetadataFactory));
-            if ($includeEnumNormalizer) {
-                $normalizers[] = new EnumNormalizer();
-            }
-            foreach ($appendNormalizers as $normalizer) {
-                $normalizers[] = $normalizer;
-            }
-            if ($includeObjectNormalizer) {
-                $normalizers[] = $objectNormalizer;
-            }
-        }
-        return $normalizers;
-
-        // list: SerializerExtractor, ReflectionExtractor, DoctrineExtractor
-        // type: Doctrine, PhpDoc, Reflection
-        // description: PhpDoc
-        // access: Doctrine, Reflection
-        // init: Reflection
     }
 
     protected function buildFixtureResponse(string $value, string $label = null)
